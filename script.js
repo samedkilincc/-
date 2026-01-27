94e14fd63b2ed60e98aef025c303895e9caced88
samedkilincc <samedkilinc46@gmail.com>
2026-01-27 11:20:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function check() {
         document.getElementById('content').classList.remove('hidden');
         
         const music = document.getElementById('music');
-        music.play().catch(e => console.log("Etkileşim gerekiyor."));
+        music.play().catch(e => console.log("Otomatik oynatma engellendi."));
     } else {
         document.getElementById('wrong').innerText = "Hiç anlamadın ki beni, hiç dediklerim seni alakadar etmedi ki bak şifreyi bile yanlış giriyorsun. Pek söze gerek yok.";
     }
