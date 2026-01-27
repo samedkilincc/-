49e19bb6f08256f5674eb3d127d7d0445a46caac
samedkilincc <samedkilinc46@gmail.com>
2026-01-27 11:04:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const DOGRU_SIFRE = "sevmek";
+
+function check() {
+    let pass = document.getElementById('password').value;
+    if(pass.toLowerCase() === DOGRU_SIFRE) {
+        document.getElementById('login').style.display = 'none';
+        document.getElementById('content').classList.remove('hidden');
+        
+        // Müzik kontrolü
+        const music = document.getElementById('music');
+        music.play().catch(error => {
+            console.log("Tarayıcı otomatik müziği engelledi, etkileşim gerekiyor.");
+        });
+    } else {
+        document.getElementById('wrong').innerText = "Hiç anlamadın ki beni, hiç dediklerim seni alakadar etmedi ki bak şifreyi bile yanlış giriyorsun. Pek söze gerek yok.";
+    }
+}
